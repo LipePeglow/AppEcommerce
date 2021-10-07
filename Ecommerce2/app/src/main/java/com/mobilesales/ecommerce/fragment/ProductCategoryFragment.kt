78c903509b6a024fc6b1d8fb4885b4db3cbe388f
Felipe Peglow Moraes <88987663+LipePeglow@users.noreply.github.com>
2021-10-07 08:06:52 -0300

    change package

--- Ecommerce2/app/src/main/java/com/mobilesales/ecommerce/fragment/ProductCategoryFragment.kt
+++ Ecommerce2/app/src/main/java/com/mobilesales/ecommerce/fragment/ProductCategoryFragment.kt
@@ -1,4 +1,4 @@
-package com.mobilesales.ecommerce
+package com.mobilesales.ecommerce.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.mobilesales.ecommerce.R
 import com.mobilesales.ecommerce.adapter.ProductCategoryAdapter
 import com.mobilesales.ecommerce.model.ProductCategory
 
