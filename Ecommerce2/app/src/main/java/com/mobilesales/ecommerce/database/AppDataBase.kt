40da7457aa54b1ab5dcc1a8f38dd299895945de9
Felipe Peglow Moraes <peglow48@gmail.com>
2021-11-05 10:24:04 -0300

    create data_base

---
+++ Ecommerce2/app/src/main/java/com/mobilesales/ecommerce/database/AppDataBase.kt
@@ -0,0 +1,48 @@
+package com.mobilesales.ecommerce.database
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mobilesales.ecommerce.model.*
+
+@Database(
+    entities = [Order::class,
+        User::class,
+        UserAddress::class,
+        ProductSize::class,
+        ProductImage::class,
+        ProductColor::class,
+        ProductCategory::class,
+        Product::class,
+        OrderedProduct::class], version = 1
+)
+
+abstract class AppDataBase : RoomDatabase() {
+
+
+
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+
+        fun getDataBase(context: Application): AppDataBase {
+
+            return INSTANCE ?: synchronized(this) {
+                var instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDataBase::class.java,
+                    "appcommerce_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .createFromAsset("appcommerce_database.db")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
