c8af71ad5ae751c099f2e383d0b3fce66b7cf8d2
Felipe Peglow Moraes <peglow48@gmail.com>
2021-11-05 16:27:28 -0300

    product dao configuration

--- Ecommerce2/app/src/main/java/com/mobilesales/ecommerce/database/ProductDao.kt
+++ Ecommerce2/app/src/main/java/com/mobilesales/ecommerce/database/ProductDao.kt
@@ -1,4 +1,22 @@
 package com.mobilesales.ecommerce.database
 
+import androidx.room.*
+import com.mobilesales.ecommerce.model.Product
+import com.mobilesales.ecommerce.model.ProductVariants
+
+@Dao
 interface ProductDao {
+
+    @Query("SELECT * FROM products WHERE categoryId = :categoryId")
+    fun loadAllByCategory(categoryId : String): List<Product>
+    @Query("SELECT * FROM products WHERE featured = 1")
+    fun loadAllFeatured(): List<Product>
+    @Transaction
+    @Query("SELECT * FROM products WHERE id = :productId")
+    fun loadProductWithVariants(productId : String): ProductVariants
+
+    @Insert
+    fun insert(product : Product)
+    @Update
+    fun update(product : Product)
 }
